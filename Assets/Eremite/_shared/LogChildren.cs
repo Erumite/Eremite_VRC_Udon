7dbe184117be63bfcc8db918dff2ebbfbbf73362
Erumite <sindern@gmail.com>
2022-03-08 08:29:00 -0600

    added a dubious little script to check children on objects at runtime.

---
+++ Assets/Eremite/_shared/LogChildren.cs
@@ -0,0 +1,37 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+// This is some hacky crap to debug certain situations where you need to list the heirarchy in-game since it can't be done in the editor.
+//  Originally meant to enumerate all the items in a VRC_PortalMarker after it has had its children populated by the VRC Client.
+
+public class LogChildren : UdonSharpBehaviour
+{
+    public GameObject objectToInspect;
+    void Start()
+    {
+        listChilden();
+    }
+    public void listChilden(){
+        var allChildren = objectToInspect.GetComponentsInChildren<Transform>();
+        foreach ( Transform child in allChildren ) {
+            Debug.Log(child.gameObject.name);
+            GetGameObjectPath(child.gameObject);
+            Component[] components = child.GetComponents(typeof(Component));
+            foreach(Component component in components) {
+                Debug.Log(" - Component: " + component.ToString());
+            }
+        }
+    }
+
+    // https://answers.unity.com/questions/8500/how-can-i-get-the-full-path-to-a-gameobject.html
+    private void GetGameObjectPath(GameObject obj) {
+        string path = "/" + obj.name;
+        while (obj.transform.parent != null) {
+            obj = obj.transform.parent.gameObject;
+            path = "/" + obj.name + path;
+        }
+        Debug.Log(path);
+    }
+}
