0fe87b90ce047ca9af63fed60fbf65ed4db71b0e
eremite <sindern@gmail.com>
2022-03-26 22:22:27 -0500

    Adding Calibration Mirror

---
+++ Assets/Eremite/CalibrationMirror/README.md
@@ -0,0 +1,9 @@
+# Calibration Mirror
+
+Super basic timed mirror with a slider to visualize time remaining.
+
+* Configurable active time.
+* Only shows the local player's mirror reflection.
+* Slider counts down before deactivating the mirror.
+
+Made for events where people might need access to a mirror to calibrate full-body, but a group of mirror-dwellers is undesirable.
---
+++ Assets/Eremite/CalibrationMirror/TempCalibrationMirror.cs
@@ -0,0 +1,48 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TempCalibrationMirror : UdonSharpBehaviour
+{
+
+    [Tooltip("Mirror that will be toggled on.")]
+    public GameObject mirror;
+    [Tooltip("Button that toggles the mirror on (for disabling).")]
+    public GameObject button;
+    [Tooltip("Slider that shows countdown before mirror disables.")]
+    public Slider timeSlider;
+    [Tooltip("How long the mirror should stay active (seconds) before disbling itself.")]
+    public float timeActive = 40;
+    // local values
+    private bool isActive = false;
+    private float activateTime = 0;
+
+    void Start()
+    {
+        mirror.SetActive(false);
+        button.SetActive(true);
+    }
+    public void showMirror() {
+        button.SetActive(false);
+        mirror.SetActive(true);
+        isActive=true;
+        activateTime=Time.realtimeSinceStartup;
+        timeSlider.value=0.0F;
+    }
+    void Update(){
+        if ( isActive == true ) {
+            float timeDiff = Time.realtimeSinceStartup - activateTime;
+            if ( timeDiff >= timeActive ) {
+                isActive = false;
+                button.SetActive(true);
+                mirror.SetActive(false);
+                timeSlider.value = 1.0F;
+            } else {
+                timeSlider.value = 1.0f - (timeDiff / timeActive);
+            }
+        }
+    }
+}
