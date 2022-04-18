a5196ce7f21370f5129ac325f47f66097643d3bc
eremite <sindern@gmail.com>
2022-04-18 06:01:53 -0500

    Added a super-simple low overhead pen with an eraser.  Uses trail renderer.

---
+++ Assets/Eremite/SuperSimplePen/SuperSimpleEraser.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class SuperSimpleEraser : UdonSharpBehaviour
+{
+    public TrailRenderer inkTrailRenderer;
+    void Start(){}
+    
+    public override void Interact()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "eraseAll");
+    }
+    public void eraseAll(){
+        inkTrailRenderer.time = 0;
+        SendCustomEventDelayedFrames("reWrite", 1, VRC.Udon.Common.Enums.EventTiming.LateUpdate);
+    }
+    public void reWrite(){
+        inkTrailRenderer.time = 99999;
+    }
+}
---
+++ Assets/Eremite/SuperSimplePen/SuperSimplePen.cs
@@ -0,0 +1,45 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+public class SuperSimplePen : UdonSharpBehaviour
+{
+    public TrailRenderer inkTrailRenderer;
+    private bool writing = false;
+    private bool localWriting = false;
+
+    void Start()
+    {
+        inkTrailRenderer.emitting = false;
+    }
+
+    public override void OnDeserialization()
+    {
+        if ( writing != localWriting ) { 
+            localWriting = writing;
+            inkTrailRenderer.emitting = localWriting;
+        }
+    }
+
+    public override void OnPickup()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+    }
+
+    public override void OnPickupUseDown()
+    {
+        writing = true;
+        RequestSerialization();
+        OnDeserialization();
+    }
+
+    public override void OnPickupUseUp()
+    {
+        writing = false;
+        RequestSerialization();
+        OnDeserialization();
+    }
+}
