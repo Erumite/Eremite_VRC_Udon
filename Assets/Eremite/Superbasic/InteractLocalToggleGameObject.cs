d3c4855af2b23861521414657eed53a18afea37b
eremite <sindern@gmail.com>
2022-08-22 16:01:02 -0500

    Adding some superbasics - optimization through hyper specialization.

---
+++ Assets/Eremite/Superbasic/InteractLocalToggleGameObject.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class InteractLocalToggleGameObject : UdonSharpBehaviour
+{
+    public GameObject[] gameObjects;
+    void Start(){}
+    public override void Interact()
+    {
+        for (int i=0; i<gameObjects.Length ; i++ ) {
+            gameObjects[i].SetActive(!gameObjects[i].activeInHierarchy);
+        }
+    }
+}
